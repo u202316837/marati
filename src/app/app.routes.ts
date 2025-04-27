7408bfd06454256eb69364bd42cd60ed4d6507eb
u202316837 <147280646+u202316837@users.noreply.github.com>
2025-04-27 15:24:31 -0500

    componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:"home", component: HomeComponent },
+    { path:"aboutus", component: AboutusComponent },
+    { path:"shop", component: ShopComponent },
+    { path:"contactus", component: ContactusComponent }
+];
