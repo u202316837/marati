eda1dda57685f61e56e6f73006d219b972c42506
u202316837 <147280646+u202316837@users.noreply.github.com>
2025-04-28 22:47:24 -0500

    detalle de productos, comentarios formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { AboutusComponent } from './pages/aboutus/aboutus.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ContactusComponent } from './pages/contactus/contactus.component';
 import { CategoryComponent } from './pages/category/category.component';
+import { ProductComponent } from './pages/product/product.component';
 
 export const routes: Routes = [
+    { path:"", redirectTo:"home", pathMatch:"full" },
     { path:"home", component: HomeComponent },
     { path:"aboutus", component: AboutusComponent },
     { path:"shop", component: ShopComponent },
     { path:"contactus", component: ContactusComponent },
-    { path:"category", component: CategoryComponent }
+    { path:"category", component: CategoryComponent },
+    { path:"product", component: ProductComponent }
 ];
---
+++ src/app/pages/product/product.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-product',
+  imports: [ReactiveFormsModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  myForm = this.fb.group({
+    nombres: ['', Validators.required],
+    correo: ['', [Validators.required, Validators.email]],
+    comentarios: ['']
+  });
+
+  submit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      alert('Su comentario ha sido enviado, sera validado para su publicacion');
+    } else {
+      alert('Por favor, complete los campos requeridos');
+    }
+  }
+}
