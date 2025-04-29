d9308c13c4772b802dc214d3a8297c60d6643786
u202316837 <147280646+u202316837@users.noreply.github.com>
2025-04-28 19:37:32 -0500

    Categorias  y formulario contactenos

--- src/app/pages/contactus/contactus.component.ts
+++ src/app/pages/contactus/contactus.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contactus.component.html',
   styleUrl: './contactus.component.css'
 })
 export class ContactusComponent {
+  constructor(
+    private fb: FormBuilder
+  ) {}
 
+  myForm = this.fb.group({
+    nombres: ['', Validators.required],
+    apellido_paterno: ['', Validators.required],
+    apellido_materno: [''],
+    documento_identidad: ['', Validators.required],
+    correo: ['', [Validators.required, Validators.email]],
+    comentarios: ['']
+  });
+
+  submit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      alert('Formulario enviado');
+    } else {
+      alert('Por favor, complete los campos requeridos');
+    }
+  }
 }
